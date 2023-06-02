b45ce8663d48ab10afadb84e2ac347db59d38042
Gabetd <gabetd0904@gmail.com>
2023-06-02 11:01:00 -0500

    feathers API

--- vue-client/main.ts
+++ vue-client/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+export const pinia = createPinia()
+
+createApp(App).use(pinia).mount('#app')
